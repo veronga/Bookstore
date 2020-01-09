789e6800a52cb6a18771721ed18e65c6c722d0d6
atlonis <veronika.belozechko@gmail.com>
2020-01-09 11:14:34 +0300

    first commit

---
+++ src/reducers/books.js
@@ -0,0 +1,25 @@
+const initialState = {
+  books: [
+    {
+      id: 0,
+      title: "Hello World!"
+    }
+  ]
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_BOOK":
+      return {
+        books: [...state.books, action.payload]
+      };
+    case "SET_BOOKS":
+      return {
+        ...state,
+        books: action.payload
+      };
+
+    default:
+      return state;
+  }
+};
