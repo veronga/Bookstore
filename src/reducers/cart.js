c46a06159138c0651199ad79443adb68808f65ec
atlonis <veronika.belozechko@gmail.com>
2020-01-10 15:33:59 +0300

    deleting books from the  cart

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -4,15 +4,15 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case "ADD_BOOK":
+    case "ADD_TO_CART":
       return {
         ...state,
         items: [...state.items, action.payload]
       };
-    case "REMOVE_BOOK":
+    case "REMOVE_FROM_CART":
       return {
         ...state,
-        item: state.items.filter(o => o.id !== action.payload)
+        items: state.items.filter(o => o.id !== action.payload)
       };
     default:
       return state;
